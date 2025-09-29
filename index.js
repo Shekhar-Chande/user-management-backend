0bcb4bcf4893fbdd5c8564c18ff8f71895acae7d
Chander Shekhar <142383542+Shekhar-Chande@users.noreply.github.com>
2025-09-29 16:20:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors'); // Essential for MERN stack communication
-const userRoutes = require('../backend/src/routes/userRoutes');
+const userRoutes = require('./src/routes/userRoutes.js');
 
 const app = express();
 const PORT = 3000;
